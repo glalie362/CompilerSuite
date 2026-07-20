5477ba21c778f50fc523190d60cc3639626b150b
Jamie <Jamie@DESKTOP-8VU4MSB>
2026-07-20 12:04:31 +0100

    instructions

--- IR/Instruction.hpp
+++ IR/Instruction.hpp
@@ -1,12 +1,39 @@
 #pragma once
+#include "Value.hpp"
 
 namespace IR {
 	template<typename Derived>
 	struct Binary {
+		Value* result;
+		Value* left;
+		Value* right;
 	};
 
 	template<typename Derived>
 	struct Unary {
+		Value* destination;
+		Value* source;
 	};
 
+
+	// Memory
+	struct Store : Unary<Store> {};
+	struct Load : Unary<Store> {};
+
+	// Math
+	struct Add : Binary <Add> {};
+	struct Subtract : Binary <Add> {};
+
+	// Control Flow
+	struct Branch {};
+	struct Jump {};
+	struct Return {};
+
+	using Instruction = std::variant<
+		Store, Load,
+
+		Add, Subtract,
+
+		Branch, Jump, Return
+	>;
 }
